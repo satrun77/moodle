437598128f961ca95c6ef86b261b056908a51b16
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2013-08-06 15:25:18 +1200

    MDL-40086 file: correct method sort_area_tree to sort files in subdirectories

--- lib/filestorage/file_storage.php
+++ lib/filestorage/file_storage.php
@@ -648,6 +648,8 @@ class file_storage {
                 collatorlib::ksort($value, collatorlib::SORT_NATURAL);
             } else if ($key == 'files') {
                 collatorlib::ksort($value, collatorlib::SORT_NATURAL);
+            } else if ($key != 'dirfile' && $key != 'dirname') {
+                $value = $this->sort_area_tree($value);
             }
         }
         return $tree;
